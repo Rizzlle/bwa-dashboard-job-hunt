7ed31b1b3acdfbc48eddb1026097a0dd41f563a4
Hariyanto <hariyan242@gmail.com>
2023-08-26 19:25:56 +0700

    update: organize layout

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface SignInPageProps {}
+
+const SignInPage: FC<SignInPageProps> = ({}) => {
+	return <div>SignInPage</div>;
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface SignUpPageProps {}
+
+const SignUpPage: FC<SignUpPageProps> = ({}) => {
+	return <div>SignUpPage</div>;
+};
+
+export default SignUpPage;
