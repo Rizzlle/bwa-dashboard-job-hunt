f5696ae0bb9c0115f31f19db0019a2059bdc8361
Hariyanto <hariyan242@gmail.com>
2023-08-27 19:21:39 +0700

    update: fixing unknown type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,6 +2,6 @@ import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
 	interface Session {
-		user: { id: string } & DefaultSession["user"];
+		user: { id: any } & DefaultSession["user"];
 	}
 }
