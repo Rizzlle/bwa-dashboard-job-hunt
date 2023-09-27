e2606469bc286de585bc679d683e59520611444e
Hariyanto <hariyan242@gmail.com>
2023-09-27 14:08:01 +0700

    update: desktop view only

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,8 +5,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
 
 const epilogue = Epilogue({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	viewport: ''
+}
 
 export default async function RootLayout({
 	children,
