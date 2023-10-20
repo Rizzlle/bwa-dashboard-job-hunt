6e76a28a8ce87432859cf9e5eba34da0f6ea4b18
rizzlle <hariyannnn@gmail.com>
2023-10-20 15:33:37 +0700

    fix: autoclose dialog modal

--- src/components/organisms/InputBenefits/DialogAddBenefit/index.tsx
+++ src/components/organisms/InputBenefits/DialogAddBenefit/index.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -15,66 +16,68 @@ import { PlusIcon } from "lucide-react";
 import React, { useRef } from "react";
 
 interface Props {
-	updateBenefits: (item: any) => void;
+  updateBenefits: (item: any) => void;
 }
 
 export default function DialogAddBenefit({ updateBenefits }: Props) {
-	const benefitRef = useRef<HTMLInputElement>(null);
-	const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const benefitRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
 
-	const handleSaveBenefit = () => {
-		const benefit = benefitRef.current?.value;
-		const description = descriptionRef.current?.value;
+  const handleSaveBenefit = () => {
+    const benefit = benefitRef.current?.value;
+    const description = descriptionRef.current?.value;
 
-		if (benefit === "" || description === "") {
-			return;
-		}
+    if (benefit === "" || description === "") {
+      return;
+    }
 
-		updateBenefits({
-			benefit,
-			description,
-		});
-	};
+    updateBenefits({
+      benefit,
+      description,
+    });
+  };
 
-	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<Button type="button" variant="outline">
-					<PlusIcon className="h-4 w-4 mr-2" />
-					Add Benefit
-				</Button>
-			</DialogTrigger>
-			<DialogContent className="sm:max-w-[425px]">
-				<DialogHeader>
-					<DialogTitle>Add Benefit</DialogTitle>
-					<DialogDescription>
-						Make a new benefit, clicks save when your done
-					</DialogDescription>
-				</DialogHeader>
-				<div className="space-y-8 mb-5">
-					<div>
-						<Label htmlFor="benefit">Benefit</Label>
-						<Input
-							id="benefit"
-							placeholder="fill your benefit..."
-							ref={benefitRef}
-						/>
-					</div>
-					<div>
-						<Label htmlFor="description">Description</Label>
-						<Textarea
-							id="description"
-							placeholder="fill your description..."
-							ref={descriptionRef}
-						/>
-					</div>
-				</div>
-				<DialogFooter>
-					<Button type="button" onClick={handleSaveBenefit}>
-						Save
-					</Button>
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
-	);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button type="button" variant="outline">
+          <PlusIcon className="h-4 w-4 mr-2" />
+          Add Benefit
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Benefit</DialogTitle>
+          <DialogDescription>
+            Make a new benefit, clicks save when your done
+          </DialogDescription>
+        </DialogHeader>
+        <div className="space-y-8 mb-5">
+          <div>
+            <Label htmlFor="benefit">Benefit</Label>
+            <Input
+              id="benefit"
+              placeholder="fill your benefit..."
+              ref={benefitRef}
+            />
+          </div>
+          <div>
+            <Label htmlFor="description">Description</Label>
+            <Textarea
+              id="description"
+              placeholder="fill your description..."
+              ref={descriptionRef}
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" onClick={handleSaveBenefit}>
+              Save
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
